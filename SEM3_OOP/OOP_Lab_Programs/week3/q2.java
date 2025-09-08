e4e27ef5f7b3bc4574829abc680f6a319d184369
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-08 12:26:31 +0400

    Update q2.java

--- SEM3_OOP/OOP_Lab_Programs/week3/q2.java
+++ SEM3_OOP/OOP_Lab_Programs/week3/q2.java
@@ -1,38 +1,40 @@
 import java.util.Scanner;
 
-class BankAccount {
-    public static void details() {
-        Scanner sc = new Scanner(System.in);
+class BankAccount{
+	private String accountNumber;
+	private double balance;
+	public static void details(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your account number: ");
+		String accountNumber = sc.nextLine();
 
-        System.out.print("Enter your account number: ");
-        int accountNumber = sc.nextInt();
+		System.out.println("Account created with balance $0.00");
+		double balance =  0.0;
 
-        System.out.println("Account created with balance $0.00");
-        double balance = 0.0;
+		System.out.print("Enter deposit amount: ");
+		double deposit = sc.nextDouble();
+		System.out.println("Deposit Successful.");
+		balance += deposit;
+		System.out.println("Account: " + accountNumber+ ", Balance : " + balance);
 
-        System.out.print("Enter deposit amount: ");
-        double deposit = sc.nextDouble();
-        System.out.println("Deposit Successful.");
-        balance += deposit;
+		System.out.print("Enter witdrawl amount: ");
+		double withdraw = sc.nextDouble();
 
-        System.out.println("Account: " + accountNumber + ", Balance: " + balance);
+		if (withdraw <= balance){
+			System.out.println("Withdrawl Successful.");
+			balance = (balance-withdraw);
+		}
+		else {
+			System.out.println("Insufficient balance.");
+		}
 
-        System.out.print("Enter withdrawal amount: ");
-        double withdraw = sc.nextDouble();
+		sc.close();
 
-        if (withdraw <= balance) {
-            System.out.println("Withdrawal Successful.");
-            balance -= withdraw;
-        } else {
-            System.out.println("No Money in Bank!");
-        }
 
-        sc.close();
-    }
+	}
 }
-
-public class q2 {
-    public static void main(String[] args) {
-        BankAccount.details();
-    }
+public class q2{
+	public static void main(String args[]){
+		BankAccount.details();
+}
 }

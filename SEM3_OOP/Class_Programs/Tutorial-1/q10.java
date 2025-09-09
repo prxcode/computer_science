0b24f91e621c9c00cff20a7b2d17586bbb80c945
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-09 10:08:29 +0400

    Create q10.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-1/q10.java
@@ -0,0 +1,10 @@
+public class Test {
+  public static void main(String[] args) {
+    int x = 10;
+    String o = Integer.toOctalString(x);
+    System.out.println(o);
+    System.out.println(Integer.toString(x, 8));
+  }
+}
+
+// Ans. b) 12 12

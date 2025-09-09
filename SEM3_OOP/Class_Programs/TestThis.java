57599966da0e81dd49e654eeb9622711d4106dc7
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-09 08:50:56 +0400

    Update TestThis.java

--- SEM3_OOP/Class_Programs/TestThis.java
+++ SEM3_OOP/Class_Programs/TestThis.java
@@ -6,17 +6,17 @@ class Student{
   String name;
   float fee;
   Student(int rollno, String name, float fee){
-    rollno = rollno;
-    name =name;
-    fee=fee;
+    this.rollno = rollno;
+    this.name = name;
+    this.fee = fee;
   }
   void display(){
-    System.out.println(rollno+"+name+"+fee);
+    System.out.println(rollno+" "+name+" "+fee);
   }
-
+}
 
 class TestThis{
-    public satic void main(String args[]){
+    public static void main(String args[]){
         Student s1 = new Student(111,"ankit",5000f);
         Student s2 = new Student(112,"sumit",6000f);
        s1.display();

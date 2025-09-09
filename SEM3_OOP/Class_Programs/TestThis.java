be93c6078276da5c018f5503bbf54c4a663d2ea0
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-09 09:06:11 +0400

    Update TestThis.java

--- SEM3_OOP/Class_Programs/TestThis.java
+++ SEM3_OOP/Class_Programs/TestThis.java
@@ -1,25 +1,26 @@
 // this keyword
 // it can be used to refer current class instance variable.
-
-class Student{
+class Student {
   int rollno;
   String name;
   float fee;
-  Student(int rollno, String name, float fee){
-    this.rollno = rollno;
-    this.name = name;
-    this.fee = fee;
+
+  Student(int r, String n, float f) {
+    this.rollno = r;
+    this.name = n;
+    this.fee = f;
   }
-  void display(){
-    System.out.println(rollno+" "+name+" "+fee);
+
+  void display() {
+    System.out.println(rollno + " " + name + " " + fee);
   }
 }
 
-class TestThis{
-    public static void main(String args[]){
-        Student s1 = new Student(111,"ankit",5000f);
-        Student s2 = new Student(112,"sumit",6000f);
-       s1.display();
-       s2.display();
-    }
+class TestThis {
+  public static void main(String[] args) {
+    Student s1 = new Student(111, "ankit", 5000f);
+    Student s2 = new Student(112, "sumit", 6000f);
+    s1.display();
+    s2.display();
+  }
 }

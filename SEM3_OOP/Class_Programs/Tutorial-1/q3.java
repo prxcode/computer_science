6a286e1f016e82e409f212b2be976a4eae860521
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-09 09:57:51 +0400

    Create q3.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-1/q3.java
@@ -0,0 +1,9 @@
+public class Test {
+  public static final int max = 100;
+    public static void main(String[] args) {
+    max++;
+    System.out.println(max);
+  }
+}
+
+// Ans. b) Compilation error

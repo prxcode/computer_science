49bc0271f5ff98054bc7fc7d8c142ca248a39e28
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-09 10:03:35 +0400

    Create q5.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-1/q5.java
@@ -0,0 +1,9 @@
+public class q5 {
+  public static void main(String[] args) {
+    String s = "--100";
+    int i = Integer.parseInt(s);
+    System.out.println(i);
+  }
+}
+
+// Ans. a) NumberFormatException

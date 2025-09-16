58e62df1a503ec3ecd1c61d7ac1982f172d5690b
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-16 09:48:57 +0400

    Create q3.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-2/q3.java
@@ -0,0 +1,11 @@
+class q3{
+  public static void main(String args[]){
+    int i = 10, j = 5;
+    if (i % j == 0 || i - j <= 3)
+      System.out.println("i is multiple of j");
+    else
+      System.out.println("i is not multiple of j");
+  }
+}
+
+// Ans. a) i is multiple of j

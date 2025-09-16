114ce64273b454f47f387d161e8b50bdd8bad27e
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-16 09:11:48 +0400

    Update Bike9.java

--- SEM3_OOP/Class_Programs/Bike9.java
+++ SEM3_OOP/Class_Programs/Bike9.java
@@ -1,10 +1,13 @@
-class Bike9{
-  final int speedlimit=90;
-  void run{
-    speedlimit=400;
-  }
-  public static void main(String args[]){
-    Bike9 obj = new Bike9();
-    obj.run();
-  }
+class Bike9 {
+    final int speedlimit = 90;  // final variable cannot be reassigned
+
+    void run() {
+        // speedlimit = 400;  // ERROR: cannot assign a value to final variable
+        System.out.println("Speed limit is: " + speedlimit);
+    }
+
+    public static void main(String args[]) {
+        Bike9 obj = new Bike9();
+        obj.run();
+    }
 }

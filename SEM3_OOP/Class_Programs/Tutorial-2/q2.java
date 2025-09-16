3c68ab5f54f452cbcdaade72f80c1569827bd010
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-16 09:46:56 +0400

    Create q2.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-2/q2.java
@@ -0,0 +1,11 @@
+class q2{
+  public static void main(String args[]){
+    int x = 5;
+    if (x > 3)
+      if (x < 10)
+        System.out.println("A");
+      else
+        System.out.println("B");
+  }
+}
+// Ans: a) A

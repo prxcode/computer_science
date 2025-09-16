5999c04a9a28d4c271ff404d09d0b310b7ec1b80
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-16 10:03:13 +0400

    Create q9.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-2/q9.java
@@ -0,0 +1,11 @@
+class q9{
+  public static void main(String args[]){
+    for (int i = 0; i < 5; i++) {
+        if (i == 3)
+          break;
+        System.out.print(i + " ");
+        }
+  }
+}
+
+// Ans: d) Nothing printed

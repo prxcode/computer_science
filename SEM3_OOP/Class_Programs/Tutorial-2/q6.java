0233dc8632b43bcaa177ab7e1d469a99d1d9e8a3
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-16 09:58:06 +0400

    Create q6.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-2/q6.java
@@ -0,0 +1,12 @@
+class q6{
+  public static void main(String args[]){
+    char c = 'b';
+  switch(c) {
+    default: System.out.println("default");
+    case 'a': System.out.println("apple");
+    case 'b': System.out.println("ball");
+    case 'c': System.out.println("cat");
+  }
+
+// Ans: a) ball
+      

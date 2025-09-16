d25538e871e772018de87bac3b4245956086240e
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-16 09:45:25 +0400

    Update q1.java

--- SEM3_OOP/Class_Programs/Tutorial-2/q1.java
+++ SEM3_OOP/Class_Programs/Tutorial-2/q1.java
@@ -1,7 +1,10 @@
-String s = "Hello";
-if (s == "Hello")
-System.out.println("Hello by ==");
-if (s.equals("Hello"))
-System.out.println("Hello by equals");
-
+class q1{
+  public static void main(String args[]){
+    String s = "Hello";
+    if (s == "Hello")
+      System.out.println("Hello by ==");
+    if (s.equals("Hello"))
+      System.out.println("Hello by equals");
+  }
+}
 // Ans: c) Hello by ==, Hello by equals

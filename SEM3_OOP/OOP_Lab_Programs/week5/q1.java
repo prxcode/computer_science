ffd10dd5237601ec27120f819fd5509737ef734c
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-22 11:58:32 +0400

    Update q1.java

--- SEM3_OOP/OOP_Lab_Programs/week5/q1.java
+++ SEM3_OOP/OOP_Lab_Programs/week5/q1.java
@@ -1,58 +1,29 @@
-
 import java.util.Scanner;
 
 public class P1 {
-
-public static void main(String args[]) {
-
-Scanner sc = new Scanner(System.in);
-
-"); System.out.print("Enter number of elements =
-
-int n = sc.nextInt();
-
-System.out.print("Enter " + n + " elements = "); int[] a = new int[n];
-
-for (int i = 0; i < n; i++) { } a[i] = sc.nextInt();
-
-
-if (n < 2) {
-
-System.out.println("Cannot find second largest number. Array must have at least 2 elements.");
-
-sc.close();
-
-} return;
-
-
-
-int firstLargest = Integer.MIN_VALUE;
-
-int secondLargest = Integer.MIN_VALUE;
-
-
-for (int i = 0; i < n; i++) {
-
-if (a[i] > firstLargest) {
-
-secondLargest = firstLargest;
-
-firstLargest = a[i];
-
-} else if (a[i] > secondLargest && a[i] <
-
-firstLargest) {
-
-secondLargest = a[i];
-
-}
-
-}
-
-System.out.println("The second largest number is: " + secondLargest);
-
-sc.close();
-
-}
-
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of elements= ");
+		int n = sc.nextInt();
+		System.out.println("Enter " + n + " elements= ");
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		if (n<2) {
+			System.out.println("Error");
+		}
+		int firstLargest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
+		for (int i=0; i<n; i++) {
+			if(arr[i] > firstLargest) {
+				secondLargest = firstLargest;
+				firstLargest = arr[i];
+			}
+			else if(arr[i] > secondLargest && arr[i] < firstLargest) {
+				secondLargest = arr[i];
+			}
+		}
+		System.out.println("Second largest element= " + secondLargest);
+	}
 }

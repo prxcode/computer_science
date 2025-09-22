377f372926a8849785a728df1c3afbc8858942c9
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-22 11:49:19 +0400

    Update q1.java

--- SEM3_OOP/OOP_Lab_Programs/week5/q1.java
+++ SEM3_OOP/OOP_Lab_Programs/week5/q1.java
@@ -1,26 +1,58 @@
-public class SecondLargestElement {
-    public static int findSecondLargest(int[] arr) {
-        if (arr.length < 2) {
-            throw new IllegalArgumentException("Array must have at least two elements.");
-        }
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
-        for (int num : arr) {
-            if (num > largest) {
-                secondLargest = largest;
-                largest = num;
-            } else if (num > secondLargest && num != largest) {
-                secondLargest = num;
-            }
-        }
-        if (secondLargest == Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("No second largest element found.");
-        }
-        return secondLargest;
-    }
-    public static void main(String[] args) {
-        int[] numbers = {10, 5, 20, 8, 15};
-        int secondLargest = findSecondLargest(numbers);
-        System.out.println("The second-largest element is: " + secondLargest);
-    }
+
+import java.util.Scanner;
+
+public class P1 {
+
+public static void main(String args[]) {
+
+Scanner sc = new Scanner(System.in);
+
+"); System.out.print("Enter number of elements =
+
+int n = sc.nextInt();
+
+System.out.print("Enter " + n + " elements = "); int[] a = new int[n];
+
+for (int i = 0; i < n; i++) { } a[i] = sc.nextInt();
+
+
+if (n < 2) {
+
+System.out.println("Cannot find second largest number. Array must have at least 2 elements.");
+
+sc.close();
+
+} return;
+
+
+
+int firstLargest = Integer.MIN_VALUE;
+
+int secondLargest = Integer.MIN_VALUE;
+
+
+for (int i = 0; i < n; i++) {
+
+if (a[i] > firstLargest) {
+
+secondLargest = firstLargest;
+
+firstLargest = a[i];
+
+} else if (a[i] > secondLargest && a[i] <
+
+firstLargest) {
+
+secondLargest = a[i];
+
+}
+
+}
+
+System.out.println("The second largest number is: " + secondLargest);
+
+sc.close();
+
+}
+
 }

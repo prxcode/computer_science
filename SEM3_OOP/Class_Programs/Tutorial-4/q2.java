2311d6f08284c9d72eea2aeededb9f7d39809215
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-30 09:29:53 +0400

    Create q2.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-4/q2.java
@@ -0,0 +1,11 @@
+public class q2 {
+  public static void main(String[] args) {
+    int[][] matrix = new int[2][];
+    matrix[0] = new int[2];
+    matrix[1] = new int[3];
+    matrix[1][2] = 50;
+    System.out.println(matrix[1][2]);
+  }
+}
+
+// Ans. B) 50

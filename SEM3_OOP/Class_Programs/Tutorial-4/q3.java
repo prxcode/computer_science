a9b58685b92d1c34bc1926514c83f2b31a838fda
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-09-30 09:30:44 +0400

    Create q3.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-4/q3.java
@@ -0,0 +1,13 @@
+public class CopyTest {
+  int x;
+    CopyTest(int i) { x = i; }
+    CopyTest(CopyTest ct) { x = ct.x; }
+    public static void main(String[] args) {
+    CopyTest a = new CopyTest(5);
+    CopyTest b = new CopyTest(a);
+    b.x = 10;
+    System.out.println(a.x);
+  }
+}
+
+// Ans A) 5

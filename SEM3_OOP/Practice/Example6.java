d8d09b2cfee5cbdc2d6598785efe39e40545834d
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-05 16:00:57 +0400

    Create Example6.java

---
+++ SEM3_OOP/Practice/Example6.java
@@ -0,0 +1,21 @@
+// Creating Array in C and Java
+// In C we write: int a[5]; a[2] = 56; printf(a[2]); -> 56
+// In Java we write: int a[] = new int [5]; a[2] = 56; System.out.println(a[2]); -> 56 also System.out.println(a.length); -> 5
+
+// You can directly write in terminal: java Example6 43 43 56 65 75 43 77 
+
+class Example6{
+	public static void main(String args[]){
+		int i, s=0;
+		for (i=0; i<args.length; i++){
+			System.out.println(i+" index: "+args[i]);
+		}
+		for (i=0; i<args.length; i++){
+			s = s+Integer.parseInt(args[i]);
+			
+		}
+		System.out.println("Sum of all numbers are: "+s);
+		System.out.println("Average is: "+s/args.length);
+		
+	}
+}

8be1ad1e0cf5029d2708a0d90d6d3d66daa3e313
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-16 09:02:45 +0400

    Update and rename q5.java to q05.java

--- SEM3_OOP/Class_Programs/Tutorial-5/q05.java
+++ SEM3_OOP/Class_Programs/Tutorial-5/q05.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class q5 {
+public class q05 {
     public static void main(String[] args) {
 ArrayList<String> list = new ArrayList<>(); list.add("A");
 list.add("B");

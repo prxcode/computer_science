515a2f29d763782265ba2d8a3bb7f6abad1fa057
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-16 09:03:11 +0400

    Update and rename q6.java to q06.java

--- SEM3_OOP/Class_Programs/Tutorial-5/q06.java
+++ SEM3_OOP/Class_Programs/Tutorial-5/q06.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class q6{
+public class q06{
     public static void main(String[] args) {
 ArrayList<Integer> list = new ArrayList<>(2); for (int i = 0; i < 10; i++) {
             list.add(i);

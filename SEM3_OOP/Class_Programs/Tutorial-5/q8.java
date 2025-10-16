eec604b91eaa93fb3139136a21dae64e810cee03
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-16 08:54:00 +0400

    Add q8.java with LinkedList operations example

---
+++ SEM3_OOP/Class_Programs/Tutorial-5/q8.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+public class q8 {
+    public static void main(String[] args) {
+LinkedList<Integer> list = new LinkedList<>(); 
+list.add(1);
+list.add(2);
+list.add(3);
+list.set(1, 10);
+list.add(2, 20);
+System.out.println(list);
+}
+}
+
+
+
+// Ans. A) [1, 10, 20, 3]

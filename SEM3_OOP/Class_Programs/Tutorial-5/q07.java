d756d9baf09afb52060151143e2cdd47213748d6
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-16 09:03:23 +0400

    Rename class from q7 to q07

--- SEM3_OOP/Class_Programs/Tutorial-5/q07.java
+++ SEM3_OOP/Class_Programs/Tutorial-5/q07.java
@@ -1,5 +1,5 @@
 import java.util.LinkedList;
-public class q7 {
+public class q07 {
     public static void main(String[] args) {
 LinkedList<String> list = new LinkedList<>(); list.add("First");
 list.add("Second");

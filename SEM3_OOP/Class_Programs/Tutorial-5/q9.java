dfed51d1752973beb94806ffbd8f64e80690a74a
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-16 08:54:51 +0400

    Create q9.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-5/q9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Test9 {
+    public static void main(String[] args) {
+ArrayList<String> list = new ArrayList<>(Arrays.asList("A", "B", "C", "D")); Iterator<String> it = list.iterator();
+while (it.hasNext()) {
+            String val = it.next();
+            if (val.equals("C")) it.remove();
+        }
+        System.out.println(list);
+    }
+}
+
+// Ans. B) [A,B,D]

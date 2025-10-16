579222ae228a5af2259df6982f37cc8cba001edf
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-16 08:51:40 +0400

    Add q6.java to demonstrate ArrayList usage

---
+++ SEM3_OOP/Class_Programs/Tutorial-5/q6.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class q6{
+    public static void main(String[] args) {
+ArrayList<Integer> list = new ArrayList<>(2); for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+System.out.println("Size: " + list.size()); }
+}
+
+// Ans. A) Size: 10

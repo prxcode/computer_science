2255dbba2bde216fc6d4af573679676e727e6103
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-10-16 08:50:51 +0400

    Add ArrayList example in q5.java
    Implement a simple ArrayList example in Java that adds and removes elements.

---
+++ SEM3_OOP/Class_Programs/Tutorial-5/q5.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class q5 {
+    public static void main(String[] args) {
+ArrayList<String> list = new ArrayList<>(); list.add("A");
+list.add("B");
+list.add(1, "C");
+        list.remove("B");
+        System.out.println(list);
+    }
+}
+// Ans. A) [A, C]

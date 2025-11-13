317995c367895af510c7e53cd38590432b2153cb
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-11-13 09:12:36 +0400

    Create q04.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-6/q04.java
@@ -0,0 +1,7 @@
+// 4. Which of the following causes a compilation error in inheritance?
+// A. Calling a superclass method in the subclass
+// B. Re-declaring a superclass method with a more restrictive access modifier 
+// C. Hiding a variable in the subclass
+// D. Using super() in subclass constructor
+
+// Ans. B) Re-declaring a superclass method with a more restrictive access modifier 

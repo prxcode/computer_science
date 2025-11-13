7ea8f67cae3dc35ebd42006a5dec11d30f99dd89
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-11-13 08:48:26 +0400

    Create q01.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-7/q01.java
@@ -0,0 +1,24 @@
+interface Drawable {
+  void draw();
+  default void color() { System.out.println("Black"); } 
+}
+abstract class Shape implements Drawable {
+  Shape() { System.out.println("Shape Created"); } 
+  abstract double area();
+}
+class Circle extends Shape {
+  private int r;
+  Circle(int r) { this.r = r; }
+  double area() { return 3.14 * r * r; }
+  public void draw() { System.out.println("Drawing Circle"); }
+}
+public class q01 {
+  public static void main(String[] args) {
+    Drawable d = new Circle(5);
+      d.draw();
+      d.color(); 
+    System.out.println(((Circle)d).area());
+  } 
+}
+
+// Ans. Shape Created -> Drawing Circle -> Black -> 78.5

a1a747183b2daeb471bdbf6645c05130140117a5
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-11-13 09:09:43 +0400

    Create q02.java

---
+++ SEM3_OOP/Class_Programs/Tutorial-6/q02.java
@@ -0,0 +1,7 @@
+// 2. Which of the following statements is true about the super keyword in Java inheritance?
+// A. super can access both private and protected members of the parent class 
+// B. super can be used to call the constructor of the grandparent class directly 
+// C. super must be the first statement in a constructor if used
+// D. super can be used to override methods of the subclass
+
+// Ans. C) super must be the first statement in a constructor if used

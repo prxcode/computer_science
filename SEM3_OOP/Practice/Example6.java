5dbceae7d68d62ec9558b20408f00066f74e90e8
Priyanshu <183171126+prxcode@users.noreply.github.com>
2025-12-09 23:07:23 +0400

    Update Example6.java

--- SEM3_OOP/Practice/Example6.java
+++ SEM3_OOP/Practice/Example6.java
@@ -2,8 +2,6 @@
 // In C we write: int a[5]; a[2] = 56; printf(a[2]); -> 56
 // In Java we write: int a[] = new int [5]; a[2] = 56; System.out.println(a[2]); -> 56 also System.out.println(a.length); -> 5
 
-// You can directly write in terminal: java Example6 43 43 56 65 75 43 77 
-
 class Example6{
 	public static void main(String args[]){
 		int i, s=0;

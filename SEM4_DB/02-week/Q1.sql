67e544eceee4468ed65c3864cd2dbe88003fd710
Priyanshu <183171126+prxcode@users.noreply.github.com>
2026-02-18 11:43:07 +0400

    Update Salesman table and adjust SQL queries

--- SEM4_DB/02-week/Q1.sql
+++ SEM4_DB/02-week/Q1.sql
@@ -3,14 +3,14 @@ CREATE TABLE Salesman(
 	id INT PRIMARY KEY,
     name VARCHAR(30),
     city VARCHAR(20),
-	commission INT);
+	commission float);
     
-    INSERT INTO Admission VALUES 
-    (123, 'Pankaj', 'Delhi', 100),
-    (456, 'Rahul', 'Mumbai', 250),
-    (789, 'Sanjay', 'Banglore', 500);
+    INSERT INTO Salesman VALUES 
+    (123, 'Pankaj', 'Delhi', 0.10),
+    (456, 'Rahul', 'Mumbai', 0.33),
+    (789, 'Sanjay', 'Banglore', 0.14);
     
-    
+ 
 CREATE TABLE Customer(
 	id INT PRIMARY KEY,
     name VARCHAR(20),
@@ -20,9 +20,10 @@ CREATE TABLE Customer(
     
 	INSERT INTO Customer VALUES 
     (122, 'Ajay', 'Kolkata', 'A', 123),
-    (455, 'Aman', 'Mumabi', 'B', 123),
+    (455, 'Aman', 'Mumbai', 'B', 123),
     (788, 'Pratham', 'Banglore', 'A', 456);
     
+   
     CREATE TABLE Orders(
 	id INT PRIMARY KEY,
     purchase_amount INT,
@@ -31,14 +32,19 @@ CREATE TABLE Customer(
     salesman_id INT);
     
 	INSERT INTO Orders VALUES 
-    (1, 599, 01-05-2002, 122, 123),
-    (2, 1499, 14-07-1998, 788, 456),
-    (3, 667, 14-02-2026, 455, 456);
+    (1, 599, '2002-05-01', 122, 123),
+    (2, 1499, '1998-07-14', 788, 456),
+    (3, 667, '2006-02-14', 455, 456);
     
     -- List salesmen who have not recieved any orders
-    SELECT salesman.name
+    SELECT name FROM Salesman s WHERE s.id NOT IN (SELECT Salesman_id FROM Orders);
     
     -- Display order where customer and salesman are from the same city
+    SELECT o.* FROM Salesman s, Customer c, Orders o WHERE o.customer_id = c.id AND o.salesman_id = s.id AND c.city = s.city;
     
     -- Find cutomer who are assigned to a salesman form a different city
+    SELECT c.name FROM Customer c, Salesman s WHERE c.salesman_id = s.id AND NOT c.city = s.city;
+    
+    -- Display orders palced through salesman with commission above 0.12
+	SELECT o.* FROM Salesman s, Orders o WHERE o.salesman_id = s.id AND s.commission > 0.12; 
     
